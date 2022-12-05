bc88486b5c0c25d382ddb9175dd602dfb3f0c638
hy <h-h@users.noreply.github.com>
2022-12-06 04:43:54 +0900

    edit

--- apex_exit.bat
+++ apex_exit.bat
@@ -4,6 +4,25 @@ set APP_NAME1=r5apex.exe
 set APP_NAME2=EasyAntiCheat.exe
 set APP_NAME3=dllhost.exe
 
+set hokan=""
+set cnumber=""
+for /f "delims=" %%i in ('tasklist /m ^| findstr dllhost') do (
+    set hokan=%%i
+    echo !hokan! | findstr "N/A" >NUL
+    if not ERRORLEVEL 1 (
+        goto ENDLOOP3;
+    )
+)
+
+:ENDLOOP3
+
+set cnumber=%hokan:~29,5%
+echo !hokan! | findstr "N/A" >NUL
+if not ERRORLEVEL 1 (
+    taskkill /pid %cnumber% /t /f
+    echo "DLLHOST killed simasita"
+)
+
 for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME1%" 2^>^&1') do (
     if /i %%i==%APP_NAME1% (
         taskkill /im %APP_NAME1% /t /f
@@ -21,25 +40,6 @@ for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME2%" 2^>^&1') do (
 )
 
 :ENDLOOP2
-
-set hokan=""
-set cnumber=""
-for /f "delims=" %%i in ('tasklist /m ^| findstr dllhost') do (
-    set hokan=%%i
-    echo !hokan! | findstr "N/A" >NUL
-    if not ERRORLEVEL 1 (
-        goto ENDLOOP3;
-    )
-)
-
-:ENDLOOP3
-
-set cnumber=%hokan:~29,5%
-echo !hokan! | findstr "N/A" >NUL
-if not ERRORLEVEL 1 (
-    echo "kill simasita"
-    taskkill /pid %cnumber% /t /f
-)
 
 setlocal DisableDelayedExpansion
 

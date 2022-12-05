b9d0369737227bb5cc0f131363bde41956640abc
hy <h-h@users.noreply.github.com>
2022-12-05 21:11:19 +0900

    edit apex bug fix2

--- apex_exit.bat
+++ apex_exit.bat
@@ -1,18 +1,21 @@
 @echo off
 set APP_NAME1=EasyAntiCheat.exe
-set APP_NAME1=r5apex.exe
+set APP_NAME2=r5apex.exe
 
 for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME1%" 2^>^&1') do (
     if /i %%i==%APP_NAME1% (
         taskkill /im %APP_NAME1% /t /f
-        goto ENDLOOP;
-    )
-)
-for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME2%" 2^>^&1') do (
-    if /i %%i==%APP_NAME2% (
-        taskkill /im %APP_NAME2% /t /f
-        goto ENDLOOP;
+        goto ENDLOOP1;
     )
 )
 
-:ENDLOOP
+:ENDLOOP1
+
+for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME2%" 2^>^&1') do (
+    if /i %%i==%APP_NAME2% (
+        taskkill /im %APP_NAME2% /t /f
+        goto ENDLOOP2;
+    )
+)
+
+:ENDLOOP2

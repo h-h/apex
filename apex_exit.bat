30ec2fb3b95a7e4e8e17cb94b1798e1fa19306a0
hy <h-h@users.noreply.github.com>
2022-12-06 02:43:57 +0900

    edit

--- apex_exit.bat
+++ apex_exit.bat
@@ -1,6 +1,8 @@
 @echo off
+setlocal enabledelayedexpansion
 set APP_NAME1=EasyAntiCheat.exe
 set APP_NAME2=r5apex.exe
+set APP_NAME3=dllhost.exe
 
 for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME1%" 2^>^&1') do (
     if /i %%i==%APP_NAME1% (
@@ -19,3 +21,26 @@ for /f %%i in ('tasklist /fi "IMAGENAME eq %APP_NAME2%" 2^>^&1') do (
 )
 
 :ENDLOOP2
+
+set hokan=""
+set cnumber=""
+for /f "delims=" %%i in ('tasklist /m ^| findstr dllhost') do (
+    set hokan=%%i
+    echo !hokan! | findstr "N/A" >NUL
+    if not ERRORLEVEL 1 (
+        goto ENDLOOP3;
+    )
+)
+
+:ENDLOOP3
+
+set cnumber=%hokan:~29,5%
+echo !hokan! | findstr "N/A" >NUL
+if not ERRORLEVEL 1 (
+    echo "kill simasita"
+    taskkill /pid %cnumber% /t /f
+)
+
+setlocal DisableDelayedExpansion
+
+exit /b 0
